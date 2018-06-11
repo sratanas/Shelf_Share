dd861eb4addaab7436453df45db21cc468d457a7
Stephanie Ratanas <stephanie.ratanas@concordusa.com>
2018-06-11 13:24:02 -0500

    update book title search

---
+++ Shelf_Share/Models/MyShelfViewModels/SearchBooksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shelf_Share.Models.MyShelfViewModels
+{
+    public class SearchBooksViewModel
+    {
+    }
+}
